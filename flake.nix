fee32a46ac5371ec99bc290306a2e820a6c77d87
JMcGuireCE <143644210+JMcGuireCE@users.noreply.github.com>
2026-04-07 23:05:56 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Telegraph development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.bashInteractive ];
+          buildInputs = with pkgs; [
+            python313
+            python313Packages.numpy
+            python313Packages.pandas
+            python313Packages.scipy
+            python313Packages.matplotlib
+          ];
+        };
+      }
+    );
+}
